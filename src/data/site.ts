58a2d90d58dc865028ec91178cb6b06322c8bd95
cij5484 <cij5484@naver.com>
2026-07-22 15:45:59 +0900

    Complete contact page and sharing metadata

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,8 +1,17 @@
 export const site = {
   artistName: 'CHO YOUN KYOUNG',
   artistRole: 'Haegeum Artist',
-  contactEmail: '',
+  title: 'CHO YOUN KYOUNG | Haegeum Artist',
+  description: '해금 연주자 조윤경의 공식 웹사이트. 공연, 프로필, 아카이브를 소개합니다.',
+  siteName: 'CHO YOUN KYOUNG',
+  locale: 'ko_KR',
+  contactEmail: 'cykguri@naver.com',
+  instagramHandle: '@cho.younkyoung',
+  instagramUrl: 'https://www.instagram.com/cho.younkyoung/',
+  inquiries: '공연 및 협업 문의',
+  affiliation: '국립부산국악원 기악단 단원',
   githubPagesUrl: 'https://cij5484.github.io/cho-youn-kyoung/',
+  shareImage: 'https://cij5484.github.io/cho-youn-kyoung/images/archive/2026-08-02-poster.png',
 } as const;
 
 export const navigationItems = [
