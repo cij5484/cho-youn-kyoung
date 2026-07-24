1f326a4870c0e4b87380cc606e49e0330b8d836b
cij5484 <cij5484@naver.com>
2026-07-24 14:50:06 +0900

    Add Sanjo hero archive actions

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -8,10 +8,11 @@ type PageTransitionProps = {
 export function PageTransition({ children }: PageTransitionProps) {
   const location = useLocation();
   const isHome = location.pathname === '/';
+  const isContact = location.pathname === '/contact';
 
   return (
     <div
-      className={`page-transition${isHome ? ' page-transition--home' : ''}`}
+      className={`page-transition${isHome ? ' page-transition--home' : ''}${isContact ? ' page-transition--contact' : ''}`}
       key={location.pathname}
     >
       {children}
